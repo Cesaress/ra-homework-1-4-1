cd7b150e62d3a8cd4ff1dec9064221368b546a89
Андрей Богданов <cesaress@yandex.ru>
2023-07-02 16:28:55 +0300

    hw 1-4-1-

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+import ConvectorColor from "./components/ConvectorColor.js";
+
+function App() {
+  return (
+    <div className="container">
+      <ConvectorColor />
+    </div>
+  );
+}
+
+export default App;
